b523d3af0e13a4d1d0b8e39809f4f6aafde8caf4
Raiver0f <r1ver0ffundm4nt@gmail.com>
2024-03-11 12:45:30 +0100

    init

---
+++ src/app/login/page.tsx
@@ -0,0 +1,12 @@
+import { AuthForm } from "@/components/auth-form";
+
+const Login = () => {
+  return (
+    <div>
+      <h1>Login</h1>
+      <AuthForm />
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/components/auth-form.tsx
@@ -0,0 +1,3 @@
+export const AuthForm = () => {
+  return <div className="bg-red-400">Auth Form</div>;
+};
